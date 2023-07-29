2c37422701f9137ed96e77479f3c01557ef9f172
Marigen Rivero <marigen@gmail.com>
2023-07-29 21:18:51 +0200

    ✨ NEW FEATURE: pages

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styleUrls: ['./by-capital-page.component.scss']
+})
+export class ByCapitalPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styleUrls: ['./by-country-page.component.scss']
+})
+export class ByCountryPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
